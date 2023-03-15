a9e89c409b23079e288bac63efb327315d20f204
danielmulangu <76424137+danielmulangu@users.noreply.github.com>
2023-03-15 16:53:33 -0500

    Draft of loging in

--- webproject-T2-0302-kaseya/src/main/java/MyServletKaseya.java
+++ webproject-T2-0302-kaseya/src/main/java/MyServletKaseya.java
@@ -1,6 +1,11 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,14 +30,58 @@ public class MyServletKaseya extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().println("Hello World");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		  String username = request.getParameter("username");
+	      String password = request.getParameter("password");
+	      authentication(username, password, response);
+	}
+	
+	void authentication (String username, String password, HttpServletResponse response) throws IOException{
+		
+		Connection connection = null;
+	    PreparedStatement preparedStatement = null;
+	    response.setContentType("text/html");
+	     PrintWriter out = response.getWriter();
+	    try {
+	         DBConnectionKaseya.getDBConnection(getServletContext());
+	         connection = DBConnectionKaseya.connection;
+	         String selectSQL = "SELECT * FROM USERS WHERE username LIKE ? AND password LIKE ?";
+	         preparedStatement = connection.prepareStatement(selectSQL);
+	         ResultSet rs = preparedStatement.executeQuery();
+	         while(rs.next()==false) {
+	        	 out.println("<BR><BR><BR><BR><BR>");               
+	                out.println("<html><head><title>Login check</title></head><body>");
+	    out.println("<CENTER><B>Unknown User</B>");
+	    out.println("<BR><BR>");
+	    out.println("<h3> Access Denied</h3></CENTER>");
+	    out.println("<BR><BR><BR>");
+	    out.println("<CENTER><Input Type=Button Value=Back></CENTER>");
+	    out.println("</body></html>");
+	         }
+	         
+	         
+	        }catch (SQLException se) {
+	            se.printStackTrace();
+	        } catch (Exception e) {
+	           e.printStackTrace();
+	        } finally {
+	           try {
+	              if (preparedStatement != null)
+	                 preparedStatement.close();
+	           } catch (SQLException se2) {
+	           }
+	           try {
+	              if (connection != null)
+	                 connection.close();
+	           } catch (SQLException se) {
+	              se.printStackTrace();
+	           }
+	        }
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
